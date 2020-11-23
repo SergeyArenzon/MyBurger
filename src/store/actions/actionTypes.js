7e8242035e27ee4ff40ede4cbb3e156b90a91ce1
SergeyArenzon <sergeyr1991@gmail.com>
2020-11-23 16:58:30 +0200

    add auth from

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -13,4 +13,7 @@ export const FETCH_ORDERS_SUCCESS = "FETCH_ORDERS_SUCCESS";
 export const FETCH_ORDERS_FAIL = "FETCH_ORDERS_FAIL";
 
 
-export const VALID_AUTH = "VALID_AUTH";
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,21 +1,21 @@
-import * as actionTypes from "../actions/actionTypes";
+// import * as actionTypes from "../actions/actionTypes";
 
 
 
-const initialState = {
-    validAuth: false
-};
+// const initialState = {
+//     validAuth: false
+// };
 
 
-const reducer = (state=initialState, action) => {
-    switch (action.types ){
-        case actionTypes.VALID_AUTH:
-            return {
-                ...state
+// const reducer = (state=initialState, action) => {
+//     switch (action.types ){
+//         case actionTypes.VALID_AUTH:
+//             return {
+//                 ...state
 
-            }
-    default:
-        return state;
-    }
+//             }
+//     default:
+//         return state;
+//     }
 
-}
+// }
