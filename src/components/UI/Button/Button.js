85c22a87e3cbcbf5c428d1dc792d32fab8c4bc38
SergeyArenzon <sergeyr1991@gmail.com>
2020-11-23 15:43:30 +0200

    fix touched input

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import classes from './Button.module.css';
+import React from "react";
+import classes from "./Button.module.css";
 
-
-const button = (props) => { 
-    return(
-        <button 
-        className={[classes.Button, classes[props.btnType]].join(' ')} 
-        disabled={props.disabled}
-        onClick={props.clicked}>
+const button = (props) => {
+    return (
+        <button
+            className={[classes.Button, classes[props.btnType]].join(" ")}
+            disabled={props.disabled}
+            onClick={props.clicked}
+        >
             {props.children}
         </button>
     );
 };
-
 
 export default button;
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
+import classes from "./Auth.module.css";
+
 class Auth extends Component {
     state = {
         controls: {
@@ -8,7 +10,7 @@ class Auth extends Component {
                 elementType: "input",
                 elementConfig: {
                     type: "email",
-                    placeholder: "Mail Adress",
+                    placeholder: "Mail Address",
                 },
                 value: "",
                 validation: {
@@ -34,12 +36,43 @@ class Auth extends Component {
             },
         },
     };
+
+    checkVaildity = (value, rules) => {
+        let isValid = true;
+        if (rules.required) {
+            isValid = value.trim() !== "" && isValid;
+        }
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid;
+    };
+
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkVaildity(
+                    event.target.value,
+                    this.state.controls[controlName].validation
+                ),
+                touched: true,
+            },
+        };
+        // console.log(this.state.controls.password)
+        this.setState({ controls: updatedControls });
+    };
     render() {
         const formElementsArray = [];
-        for (let key in this.state.orderForm) {
+        for (let key in this.state.controls) {
             formElementsArray.push({
                 id: key,
-                config: this.state.orderForm[key],
+                config: this.state.controls[key],
             });
         }
 
@@ -51,14 +84,15 @@ class Auth extends Component {
                 value={formElement.config.value}
                 invalid={!formElement.config.valid}
                 shouldValidate={formElement.config.validation}
-                touch={formElement.config.touched}
+                touched={formElement.config.touched}
                 changed={(event) =>
-                    this.inputChangedHandler(event, formElement)
+                    this.inputChangedHandler(event, formElement.id)
                 }
             />
         ));
+
         return (
-            <div>
+            <div className={classes.Auth}>
                 <form>
                     {form}
                     <Button btnType={"Success"}>SUBMIT</Button>
