a3e6cf0f31cf8bf1f0df073ef0989018af4c22eb
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-02 11:13:13 +0200

    add deploy

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,56 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 
-
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-
-
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
+
+app.use(cors()); // Cors middlware
+app.use(express.json()); // Parsing json
+
+if (process.env.NODE_ENV === "production") {
+    //serve up production assets
+    app.use(express.static("../build"));
+    // let the react app to handle any unknown routes // serve up the index.html if express does'nt recognize the route
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../", "build", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 5000;
 
-app.use(cors());          // Cors middlware
-app.use(express.json());  // Parsing json
+
+/////////////////////////////////////////////////////////////
+////////////////// Mongoose Configurations //////////////////
+/////////////////////////////////////////////////////////////
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  })
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
     console.log("MongoDB database connection established successfully");
 });
 
+const ordersRouter = require("./routes/orders");
+app.use("/orders", ordersRouter);
 
-const ordersRouter = require('./routes/orders');
-app.use('/orders', ordersRouter);
+const ingredientsRouter = require("./routes/ingredients");
+app.use("/ingredients", ingredientsRouter);
 
-const ingredientsRouter = require('./routes/ingredients');
-app.use('/ingredients', ingredientsRouter);
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
 
-const usersRouter = require('./routes/users');
-app.use('/users', usersRouter);
-
-const authRouter = require('./routes/auth');
-app.use('/auth', authRouter);
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
-    console.log('Server is running on port: ' + port);
+    console.log("Server is running on port: " + port);
 });
-
