d06b9c6f2d9b5c0d40873a231c409e88aa66202b
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-02 16:59:46 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json()); // Parsing json
 
 if (process.env.NODE_ENV === "production") {
     //serve up production assets
-    app.use(express.static("../build"));
+    app.use(express.static("build"));
     // let the react app to handle any unknown routes // serve up the index.html if express does'nt recognize the route
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "build", "index.html"));
