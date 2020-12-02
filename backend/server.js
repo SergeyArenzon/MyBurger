cca8ee2bf0462ca164f64324699edb325c468b58
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-02 16:56:20 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("../build"));
     // let the react app to handle any unknown routes // serve up the index.html if express does'nt recognize the route
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../", "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "build", "index.html"));
     });
 
       
