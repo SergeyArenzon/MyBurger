f952e6a75603b94645237ba0ed5474fe8a504e1d
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-16 12:37:01 +0200

    add React.lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React, { Component, useEffect } from "react";
+import React, {  useEffect, Suspense } from "react";
 import "./App.module.css";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
-import { Route, Switch } from "react-router-dom";
-import Auth from "./containers/Auth/Auth";
+// import Orders from "./containers/Orders/Orders";
+import { Route, Switch, Redirect } from "react-router-dom";
+// import Auth from "./containers/Auth/Auth";
 import { loadUser } from "./store/actions/auth";
+import { connect } from "react-redux";
 
 // class App extends Component {
 //     componentDidMount() {
@@ -30,27 +31,67 @@ import { loadUser } from "./store/actions/auth";
 //     }
 // }
 
+// const Checkout = React.lazy(() => {
+//   return import("./containers/Checkout/Checkout");
+// });
+
+const Orders = React.lazy(() => {
+  return import("./containers/Orders/Orders");
+});
+
+const Auth = React.lazy(() => {
+  return import("./containers/Auth/Auth");
+});
+
 const app = (props) => {
-  // componentDidMount() {
-  //     
-  // }
   useEffect(() => {
-      props.store.dispatch(loadUser());
-  }, [])
+    props.store.dispatch(loadUser());
+  }, []);
+
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={() => <Auth />} />
+      <Route path="/checkout" component={Checkout} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
+      <Switch>
+        <Route path="/" exact component={BurgerBuilder} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/auth" render={() => <Auth />} />
+        <Route path="/orders" render={() => <Orders />} />
+      </Switch>
+    );
+  }
 
   return (
-      
-    <div>
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={BurgerBuilder} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/orders" component={Orders} />
-        </Switch>
-      </Layout>
-    </div>
+    // <div>
+    //   <Layout>
+    //     <Switch>
+    //       <Route path="/" exact component={BurgerBuilder} />
+    //       <Route path="/checkout" component={Checkout} />
+    //       <Route path="/auth" component={Auth} />
+    //       <Route path="/orders" component={Orders} />
+    //     </Switch>
+    //   </Layout>
+    // </div>
+
+    <Layout>
+      <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+    </Layout>
   );
 };
 
-export default app;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    error: state.error,
+    isLoading: state.auth.isLoading,
+  };
+};
+
+export default connect(mapStateToProps)(app);
