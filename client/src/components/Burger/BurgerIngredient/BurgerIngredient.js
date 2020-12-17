996bbdb10a433fc9cfc7cf866edf9a2e1eccc454
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-17 11:53:54 +0200

    add hooks to BurgerIngredients

--- client/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ client/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,47 +1,46 @@
 /* eslint-disable default-case */
-import React, { Component } from 'react';
-import classes from './BurgerIngredient.module.css';
-import PropTypes from 'prop-types';
+import React  from "react";
+import classes from "./BurgerIngredient.module.css";
+import PropTypes from "prop-types";
 
-class BurgerIngredient extends Component {
-    render() {
-        let ingredient = null;
+const burgerIngredient = (props) => {
+  let ingredient = null;
 
-        switch( this.props.type ) {
-            case('bread-bottom'):
-                ingredient = <div className={classes.BreadBottom}></div>
-                break;
-            case('bread-top'):
-                ingredient = (
-                    <div className={classes.BreadTop}>
-                        <div className={classes.Seeds1}></div>
-                        <div className={classes.Seeds2}></div>
-                    </div>
-                ); 
-                break;
-            case('meat'):      
-                ingredient = <div className={classes.Meat}></div>
-                break;
-            case('cheese'):      
-            ingredient = <div className={classes.Cheese}></div>
-            break;  
-            case('salad'):      
-                ingredient = <div className={classes.Salad}></div>
-                break;  
-            case('bacon'):      
-                ingredient = <div className={classes.Bacon}></div>
-                break;
-            case('onion'):
-                ingredient = <div className={classes.Onion}></div>
-                break;    
-            default:
-                ingredient = null;    
-        }
-    return ingredient;
-    }
-} 
-BurgerIngredient.propTypes = {
-    type: PropTypes.string.isRequired
+  switch (props.type) {
+    case "bread-bottom":
+      ingredient = <div className={classes.BreadBottom}></div>;
+      break;
+    case "bread-top":
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
+        </div>
+      );
+      break;
+    case "meat":
+      ingredient = <div className={classes.Meat}></div>;
+      break;
+    case "cheese":
+      ingredient = <div className={classes.Cheese}></div>;
+      break;
+    case "salad":
+      ingredient = <div className={classes.Salad}></div>;
+      break;
+    case "bacon":
+      ingredient = <div className={classes.Bacon}></div>;
+      break;
+    case "onion":
+      ingredient = <div className={classes.Onion}></div>;
+      break;
+    default:
+      ingredient = null;
+  }
+  return ingredient;
 };
 
-export default BurgerIngredient;
+burgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+
+export default burgerIngredient;
