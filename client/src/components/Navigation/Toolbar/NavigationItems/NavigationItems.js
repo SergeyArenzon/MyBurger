615457bec788fda6a9217594b04d657f910f6552
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-17 11:57:34 +0200

    add hooks to NavigationItems

--- client/src/components/Navigation/Toolbar/NavigationItems/NavigationItems.js
+++ client/src/components/Navigation/Toolbar/NavigationItems/NavigationItems.js
@@ -1,31 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import { connect } from "react-redux";
 
-class NavigationItems extends Component {
-    render() {
-        return (
-            <ul className={classes.NavigationItems}>
-                <NavigationItem link="/" exact>
-                    Burger Builder
-                </NavigationItem>
-                {this.props.isAuthenticated ? (
-                    <NavigationItem link="/orders">Orders</NavigationItem>
-                ) : null}
+const navigationItems = (props) => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link="/" exact>
+        Burger Builder
+      </NavigationItem>
+      {props.isAuthenticated ? (
+        <NavigationItem link="/orders">Orders</NavigationItem>
+      ) : null}
 
-                <NavigationItem link="/auth">
-                    {this.props.isAuthenticated ? "Logout" : "Authenticate"}
-                </NavigationItem>
-            </ul>
-        );
-    }
-}
-
-const mapStateToProps = (state) => {
-    return {
-        isAuthenticated: state.auth.isAuthenticated,
-    };
+      <NavigationItem link="/auth">
+        {props.isAuthenticated ? "Logout" : "Authenticate"}
+      </NavigationItem>
+    </ul>
+  );
 };
 
-export default connect(mapStateToProps)(NavigationItems);
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+  };
+};
+
+export default connect(mapStateToProps)(navigationItems);
