5ffe79a650cc9966df9415d69fc3cbc8227cdaf3
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-23 10:40:34 +0200

    fix home button

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -1,33 +1,33 @@
-import React, {useEffect} from "react";
+import React from "react";
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-    let transformedIngredients = Object.keys(props.ingredients)
-        .map((igKey) => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey + i} type={igKey} />;
-            });
-        })
-        .reduce((arr, el) => {
-            return arr.concat(el);
-        }, []);
-        const addIngsText = <h2 className={classes.AddIngsText}>Add Ingredients</h2>
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  const addIngsText = <h2 className={classes.AddIngsText}>Add Ingredients</h2>;
 
-        /* Fixes scrolling after too many ingredients added */
-        let Burger = classes.Burger;
-        if(transformedIngredients.length > 6) {
-            Burger = classes.BurgerScrolled;
-        }
-        
-    return (
-        <div className={Burger}>
-            <BurgerIngredient type="bread-top" />
-            {transformedIngredients.length < 1 ? addIngsText : null}
-            {transformedIngredients}
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+  /* Fixes scrolling after too many ingredients added */
+  let Burger = classes.Burger;
+  if (transformedIngredients.length > 6) {
+    Burger = classes.BurgerScrolled;
+  }
+
+  return (
+    <div className={Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients.length < 1 ? addIngsText : null}
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
--- client/src/components/UI/Button/HomeButton.js
+++ client/src/components/UI/Button/HomeButton.js
@@ -2,17 +2,18 @@ import React from "react";
 import classes from "./HomeButton.module.css";
 
 const homeButton = (props) => {
-    let btn = classes.HomeButton;
-    let btnReturn = (<a className={btn} 
-        onClick={props.click}
-        >Order Now!</a>);
-    if(props.disabled) {
-        btn = [classes.HomeButton, classes.HomeButtonDisabled].join(' ');
-        btnReturn = (<a className={btn} 
-            >Order Now!</a>);
-    }
+  let btn = classes.HomeButton;
+  let btnReturn = (
+    <a href="/#" className={btn} onClick={props.click}>
+      Order Now!
+    </a>
+  );
+  if (props.disabled) {
+    btn = [classes.HomeButton, classes.HomeButtonDisabled].join(" ");
+    btnReturn = <a href="/#" className={btn} >Order Now!</a>;
+  }
 
-  return btnReturn
+  return btnReturn;
 };
 
 export default homeButton;
