abba01719538bd97338fd09160e2bdeb0e8ca6e0
SergeyArenzon <sergeyr1991@gmail.com>
2020-12-23 10:49:15 +0200

    add HomeButton title

--- client/src/components/UI/Button/HomeButton.js
+++ client/src/components/UI/Button/HomeButton.js
@@ -4,13 +4,13 @@ import classes from "./HomeButton.module.css";
 const homeButton = (props) => {
   let btn = classes.HomeButton;
   let btnReturn = (
-    <a href="/#" className={btn} onClick={props.click}>
+    <a href="/#" className={btn} onClick={props.click} >
       {props.children}
     </a>
   );
   if (props.disabled) {
     btn = [classes.HomeButton, classes.HomeButtonDisabled].join(" ");
-    btnReturn = <a href="/#" className={btn} >{props.children}</a>;
+    btnReturn = <a href="/#" className={btn} title="Add ingredients first">{props.children}</a>;
   }
 
   return btnReturn;
