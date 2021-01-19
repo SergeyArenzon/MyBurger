9e5038cdf368261996ab56e30a91009becf2507c
SergeyArenzon <sergeyr1991@gmail.com>
2021-01-19 12:06:47 +0200

    output only ing.amount > 0

--- client/src/components/Order/Order.js
+++ client/src/components/Order/Order.js
@@ -2,37 +2,41 @@ import React from "react";
 import classes from "./Order.module.css";
 
 const order = (props) => {
-  const ingredients = [];
-  for (let ingredientName in props.ingredients) {
-    ingredients.push({
-      name: ingredientName,
-      amount: props.ingredients[ingredientName],
+    const ingredients = [];
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName],
+        });
+    }
+
+    const ingredientOutput = ingredients.map((ig) => {
+        if (ig.amount > 0) {
+            return (
+                <span
+                    key={ig.name}
+                    style={{
+                        textTransform: "capitalize",
+                        display: "inline",
+                        margin: "0 8px",
+                        border: "1px solid #ccc",
+                        padding: "5px",
+                    }}
+                >
+                    {ig.name} ({ig.amount})
+                </span>
+            );
+        }
     });
-  }
 
-  const ingredientOutput = ingredients.map((ig) => {
     return (
-      <span
-        key={ig.name}
-        style={{
-          textTransform: "capitalize",
-          display: "inline",
-          margin: "0 8px",
-          border: "1px solid #ccc",
-          padding: "5px",
-        }}
-      >
-        {ig.name} ({ig.amount})
-      </span>
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>
+                Price: <strong>{props.price}</strong>
+            </p>
+        </div>
     );
-  });
-
-  return (
-    <div className={classes.Order}>
-      <p>Ingredients: {ingredientOutput}</p>
-      <p>Price: <strong>{props.price}</strong></p>
-    </div>
-  );
 };
 
 export default order;
