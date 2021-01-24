3e9577248519532d45ec0ed338977168ed6c008a
SergeyArenzon <sergeyr1991@gmail.com>
2021-01-24 14:33:25 +0200

    add redirect to / by click on Logo

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useCallback} from "react";
+import { useHistory } from "react-router-dom";
 
 import burgerlogo from "../../assets/images/burger-logo.png";
 
-import classes from './Logo.module.css';
+import classes from "./Logo.module.css";
 
-const logo = (props) => (
-  <div className={classes.Logo} >
-      <img src={burgerlogo} alt="burger logo"/>
-  </div>
-);
+const logo = (props) => {
+
+  // REDIRECT BY CLICKING ON LOGO 
+  const history = useHistory();
+  const handleOnClick = useCallback(() => history.push('/'), [history]);
 
 
-
+    return (
+        <div className={classes.Logo} onClick={handleOnClick}>
+            <img src={burgerlogo} alt="burger logo" />
+        </div>
+    );
+};
 
 export default logo;
